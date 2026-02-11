1d59d819bfe7677b910b4ea7242683375e1b1450
hemanthsaichikyala-dev <hemanthsaichikyala@gmail.com>
2026-02-11 14:12:08 +0530

    Time: 3 ms (64.13%) | Memory: 19.4 MB (16.55%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,11 +1,3 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        odd = 1
-        count = 0
-        
-        while x >= odd:
-            x -= odd
-            odd += 2
-            count += 1
-        
-        return count
+        return int(x**(1/2))
